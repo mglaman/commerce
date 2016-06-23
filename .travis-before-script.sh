6a5ae60392c33e944d4c76ff9fb9002d76f36bdf
Matt Glaman <nmd.matt@gmail.com>
2016-06-23 07:02:28 -0500

    Use disk cache with PhhantomJS

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -35,4 +35,4 @@ composer update -n --lock --verbose
 drush en -y commerce commerce_product commerce_order commerce_checkout
 
 # Turn on PhantomJS for functional Javascript tests
-phantomjs --ssl-protocol=any --ignore-ssl-errors=true $DRUPAL_TI_DRUPAL_DIR/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 >> /dev/null &
+phantomjs --disk-cache=true --ssl-protocol=any --ignore-ssl-errors=true $DRUPAL_TI_DRUPAL_DIR/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 >> /dev/null &
