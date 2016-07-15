4a6edc001c83bea6517cc670068da441618cb4d3
Matt Glaman <nmd.matt@gmail.com>
2016-07-14 22:56:09 -0700

    Fix payment backreference to order

--- modules/payment/src/Entity/Payment.php
+++ modules/payment/src/Entity/Payment.php
@@ -196,13 +196,6 @@ class Payment extends ContentEntityBase implements PaymentInterface {
       ->setSetting('target_type', 'commerce_payment_method')
       ->setReadOnly(TRUE);
 
-    // @todo Order should modify and add this?
-//    $fields['order_id'] = BaseFieldDefinition::create('entity_reference')
-//      ->setLabel(t('Order'))
-//      ->setDescription(t('The parent order.'))
-//      ->setSetting('target_type', 'commerce_order')
-//      ->setReadOnly(TRUE);
-
     $fields['remote_id'] = BaseFieldDefinition::create('string')
       ->setLabel(t('Remote ID'))
       ->setDescription(t('The remote payment ID.'))
