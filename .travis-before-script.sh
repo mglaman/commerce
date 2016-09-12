bf238c2cd22f2793a41a3f0935e1d39079c0bf59
Matt Glaman <nmd.matt@gmail.com>
2016-09-12 13:59:46 -0500

    Why is travis failing.

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -2,23 +2,8 @@
 
 set -e $DRUPAL_TI_DEBUG
 
-# Ensure the right Drupal version is installed.
-# Note: This function is re-entrant.
-drupal_ti_ensure_drupal
-
-# @todo Patch drupal_ti to not link, but add self as VCS and require.
-if [ -L "$DRUPAL_TI_MODULES_PATH/$DRUPAL_TI_MODULE_NAME" ]
-then
-  unlink "$DRUPAL_TI_MODULES_PATH/$DRUPAL_TI_MODULE_NAME"
-fi
-
 # Add custom modules to drupal build.
 cd "$DRUPAL_TI_DRUPAL_DIR"
-
-composer config repositories.commerce path $TRAVIS_BUILD_DIR
-composer require drupal/commerce:2.x-dev
-
-composer update -n --lock --verbose
 
 # Enable main module and submodules.
 drush en -y commerce commerce_product commerce_order commerce_checkout
