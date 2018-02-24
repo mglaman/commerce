6a9d0289a8469c85d5926bd12b1af48e0e49ab4e
Matt Glaman <nmd.matt@gmail.com>
2018-02-23 23:17:52 -0600

    Patch 12

--- modules/product/tests/src/FunctionalJavascript/ProductVariationAjaxChangeTest.php
+++ modules/product/tests/src/FunctionalJavascript/ProductVariationAjaxChangeTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\commerce_product\FunctionalJavascript;
 
+use Drupal\commerce_price\Entity\Currency;
 use Drupal\Tests\commerce\FunctionalJavascript\JavascriptTestTrait;
 use Drupal\Tests\commerce_product\Functional\ProductBrowserTestBase;
 use Drupal\commerce_price\Price;
@@ -11,7 +12,7 @@ use Drupal\commerce_price\Price;
  *
  * @group commerce
  */
-class ProductAddToCartVariationChangeJavascriptTest extends ProductBrowserTestBase {
+class ProductVariationAjaxChangeTest extends ProductBrowserTestBase {
 
   use JavascriptTestTrait;
 
@@ -20,21 +21,21 @@ class ProductAddToCartVariationChangeJavascriptTest extends ProductBrowserTestBa
    *
    * @var \Drupal\commerce_product\Entity\ProductVariation
    */
-  protected $low_bulb;
+  protected $lowBulb;
 
   /**
    * The 'high' product variant.
    *
    * @var \Drupal\commerce_product\Entity\ProductVariation
    */
-  protected $high_bulb;
+  protected $highBulb;
 
   /**
    * {@inheritdoc}
    */
   public static $modules = [
     'commerce_cart',
-    'commerce_product_add_to_cart_variation_change',
+    'commerce_product_variation_ajax_change_test',
   ];
 
   /**
@@ -42,7 +43,6 @@ class ProductAddToCartVariationChangeJavascriptTest extends ProductBrowserTestBa
    */
   protected function setUp() {
     parent::setUp();
-    $defaultCurrency = $this->store->getDefaultCurrencyCode();
 
     // Create test attribute values.
     $low = $this->createEntity('commerce_product_attribute_value', [
@@ -55,72 +55,81 @@ class ProductAddToCartVariationChangeJavascriptTest extends ProductBrowserTestBa
     ]);
 
     // Create test commerce_product.
-    $product = $this->createEntity('commerce_product', [
+    $this->product = $this->createEntity('commerce_product', [
       'type' => 'default',
       'title' => 'Bulb',
+      'stores' => [$this->store],
     ]);
-    $product->setStores([$this->store]);
 
     // Create test commerce_product_variations.
-    $low_bulb = $this->createEntity('commerce_product_variation', [
+    $this->lowBulb  = $this->createEntity('commerce_product_variation', [
       'title' => 'Bulb - Low',
       'type' => 'default',
       'sku' => 'bulb-low',
-      'product_id' => $product->id(),
+      'product_id' => $this->product->id(),
+      'price' => [
+        'number' => 10,
+        'currency_code' => $this->store->getDefaultCurrencyCode(),
+      ],
+      'attribute_brightness' => $low->id()
     ]);
-    $low_bulb
-      ->set('price', new Price('10', $defaultCurrency))
-      ->set('attribute_brightness', $low->id())
-      ->save();
 
-    $high_bulb = $this->createEntity('commerce_product_variation', [
+    $this->highBulb = $this->createEntity('commerce_product_variation', [
       'title' => 'Bulb - High',
       'type' => 'default',
       'sku' => 'bulb-high',
-      'product_id' => $product->id(),
+      'product_id' => $this->product->id(),
+      'price' => [
+        'number' => 20,
+        'currency_code' => $this->store->getDefaultCurrencyCode(),
+      ],
+      'attribute_brightness' => $high->id()
     ]);
-    $high_bulb
-      ->set('price', new Price('20', $defaultCurrency))
-      ->set('attribute_brightness', $high->id())
-      ->save();
 
-    $product->setVariations([$low_bulb, $high_bulb])->save();
-
-    $this->product = $product;
-    $this->low_bulb = $low_bulb;
-    $this->high_bulb = $high_bulb;
+    $this->product->setVariations([
+      $this->lowBulb,
+      $this->highBulb
+    ])->save();
   }
 
   /**
    * Tests managing product attribute values.
+   *
+   * The `commerce_product_variation.default.full` configuration uses the
+   * `commerce_price_plain` formatter, but the default view mode still uses the
+   * `commerce_price_default` formatter. The AJAX refresh should return currency
+   *  in the plain format of ##.00 USD and not $##.00.
    */
   public function testAddToCartVariationChange() {
+    $this->drupalGet($this->product->toUrl());
+
     $session = $this->getSession();
     $assert_session = $this->assertSession();
     $page = $session->getPage();
+    $render = $this->container->get('renderer');
 
-    $product_id = $this->product->id();
-    $product_url = $this->product->toUrl()->toString();
-    $low_bulb_id = $this->low_bulb->id();
-    $low_bulb_price = implode(' ', [
-      number_format($this->low_bulb->getPrice()->getNumber(), 2, '.', ','),
-      $this->low_bulb->getPrice()->getCurrencyCode(),
-    ]);
-    $high_bulb_id = $this->high_bulb->id();
-    $high_bulb_price = implode(' ', [
-      number_format($this->high_bulb->getPrice()->getNumber(), 2, '.', ','),
-      $this->high_bulb->getPrice()->getCurrencyCode(),
-    ]);
-    $price_field_selector = '.product--variation-field--variation_price__' . $product_id;
+    $low_bulb_id = $this->lowBulb->id();
+    $low_bulb_price = [
+      '#theme' => 'commerce_price_plain',
+      '#number' => $this->lowBulb->getPrice()->getNumber(),
+      '#currency' => Currency::load($this->lowBulb->getPrice()->getCurrencyCode()),
+    ];
+    $low_bulb_price = trim($render->renderPlain($low_bulb_price));
+    $high_bulb_id = $this->highBulb->id();
+    $high_bulb_price = [
+      '#theme' => 'commerce_price_plain',
+      '#number' => $this->highBulb->getPrice()->getNumber(),
+      '#currency' => Currency::load($this->highBulb->getPrice()->getCurrencyCode()),
+    ];
+    $high_bulb_price = trim($render->renderPlain($high_bulb_price));
+    $price_field_selector = '.product--variation-field--variation_price__' . $this->product->id();
 
-    $this->drupalGet($product_url);
     $assert_session->elementExists('css', $price_field_selector);
     $assert_session->elementTextContains('css', $price_field_selector . ' .field__item', $low_bulb_price);
     $assert_session->selectExists('purchased_entity[0][attributes][attribute_brightness]');
     $assert_session->optionExists('purchased_entity[0][attributes][attribute_brightness]', $low_bulb_id);
     $assert_session->optionExists('purchased_entity[0][attributes][attribute_brightness]', $high_bulb_id);
     $assert_session->fieldValueEquals('purchased_entity[0][attributes][attribute_brightness]', $low_bulb_id);
-
     $page->selectFieldOption('purchased_entity[0][attributes][attribute_brightness]', $high_bulb_id);
     $assert_session->assertWaitOnAjaxRequest();
     $assert_session->elementExists('css', $price_field_selector);
