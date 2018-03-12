55e3a8f0a36e3dc0b4fb36a5f15bb409288626bd
Matt Glaman <nmd.matt@gmail.com>
2018-03-12 14:46:33 -0500

    Update and tweak test

--- modules/cart/tests/src/FunctionalJavascript/AddToCartOptionalAttributeTest.php
+++ modules/cart/tests/src/FunctionalJavascript/AddToCartOptionalAttributeTest.php
@@ -9,7 +9,7 @@ use Drupal\Tests\commerce_cart\Functional\CartBrowserTestBase;
 /**
  * Tests the add to cart form.
  *
- * @group failing
+ * @group commerce
  */
 class AddToCartOptionalAttributeTest extends CartBrowserTestBase {
 
@@ -93,6 +93,8 @@ class AddToCartOptionalAttributeTest extends CartBrowserTestBase {
   /**
    * Tests add-to-cart form where variation have mutually exclusive attributes.
    *
+   * @group debug
+   *
    * @see https://www.drupal.org/node/2730643
    */
   public function testMutuallyExclusiveAttributeMatrixTwoByTwobyTwo() {
@@ -101,22 +103,22 @@ class AddToCartOptionalAttributeTest extends CartBrowserTestBase {
 
     // All attribute-groups have an 'x' value that stand for 'empty'.
     $number_attributes = $this->createAttributeSet($variation_type, 'number', [
-      'x' => 'x',
       'one' => 'one',
+      'two' => 'two',
     ]);
     $greek_attributes = $this->createAttributeSet($variation_type, 'greek', [
-      'x' => 'x',
       'alpha' => 'alpha',
+      'omega' => 'omega',
     ]);
     $city_attributes = $this->createAttributeSet($variation_type, 'city', [
-      'x' => 'x',
       'milano' => 'milano',
+      'pancevo' => 'pancevo',
     ]);
 
     $attribute_values_matrix = [
-      ['one', 'x', 'x'],
-      ['x', 'alpha', 'x'],
-      ['x', 'x', 'milano'],
+      ['one', 'omega', 'pancevo'],
+      ['two', 'alpha', 'pancevo'],
+      ['two', 'omega', 'milano'],
     ];
 
     // Generate variations from the attribute-matrix.
@@ -148,27 +150,46 @@ class AddToCartOptionalAttributeTest extends CartBrowserTestBase {
     $city_selector = 'purchased_entity[0][attributes][attribute_city]';
 
 
-    // Initial state: ['one', 'x', 'x'].
+    // Initial state: ['one', 'omega', 'pancevo'].
     $this->drupalGet($product->toUrl());
     $this->assertAttributeSelected($number_selector, 'one');
-    $this->assertAttributeSelected($greek_selector, 'x');
-    $this->assertAttributeSelected($city_selector, 'x');
+    $this->assertAttributeSelected($greek_selector, 'omega');
+    $this->assertAttributeSelected($city_selector, 'pancevo');
 
-    // Use AJAX to change the number-attribute to 'x'.
+    // Use AJAX to change the number-attribute to 'two'.
     $this->drupalGet($product->toUrl());
-    $this->getSession()->getPage()->selectFieldOption($number_selector, 'x');
+    $this->getSession()->getPage()->selectFieldOption($number_selector, 'two');
     $this->waitForAjaxToFinish();
+    $this->saveHtmlOutput();
 
+    // New state: ['two', 'alpha', 'pancevo'].
+    // The top level attribute was adjusted, so the options are reset.
+    $this->assertAttributeSelected($number_selector, 'two');
+    $this->assertAttributeSelected($greek_selector, 'alpha');
+    $this->assertAttributeSelected($city_selector, 'pancevo');
 
-    // Issues arise here...
-    // New state: ['x', ?, ?].
-    $this->assertAttributeSelected($number_selector, 'x');
-    $this->assertAttributeSelected($greek_selector, 'x');
-    $this->assertAttributeSelected($city_selector, 'x');
-    // Expect all options can be chosen now?
     $this->assertAttributeExists($number_selector, $number_attributes['one']->id());
-    $this->assertAttributeExists($greek_selector, $greek_attributes['alpha']->id());
-    $this->assertAttributeExists($city_selector, $city_attributes['milano']->id());
+    $this->assertAttributeExists($greek_selector, $greek_attributes['omega']->id());
+    $this->assertAttributeExists($city_selector, $city_attributes['pancevo']->id());
+
+    // Use AJAX to change the number-attribute to 'two'.
+    $this->drupalGet($product->toUrl());
+    $this->getSession()->getPage()->selectFieldOption($greek_selector, 'omega');
+    $this->waitForAjaxToFinish();
+    $this->saveHtmlOutput();
+
+    // New state: ['one', 'omega', 'pancevo'].
+    // The top level attribute was adjusted, so the options are reset.
+    $this->assertAttributeSelected($number_selector, 'one');
+    $this->assertAttributeSelected($greek_selector, 'omega');
+    $this->assertAttributeSelected($city_selector, 'pancevo');
+
+    $this->assertAttributeExists($number_selector, $number_attributes['two']->id());
+    $this->assertAttributeDoesNotExist($greek_selector, $greek_attributes['alpha']->id());
+    // We should not be able to change the city.
+    // There is one variation with "one" and "omega", which means there is only
+    // one city option.
+    $this->assertAttributeDoesNotExist($city_selector, $city_attributes['milano']->id());
   }
 
 }
