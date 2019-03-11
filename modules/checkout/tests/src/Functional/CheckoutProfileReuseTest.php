ac1ccecaefea09b826d97219565094655fe10994
Matt Glaman <nmd.matt@gmail.com>
2019-03-11 11:51:31 -0500

    Verify the profile is reused and a version is copied to the order

--- modules/checkout/tests/src/Functional/CheckoutProfileReuseTest.php
+++ modules/checkout/tests/src/Functional/CheckoutProfileReuseTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\commerce_checkout\Functional;
 
+use Drupal\commerce_order\Entity\Order;
 use Drupal\Core\Url;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
@@ -121,6 +122,30 @@ class CheckoutProfileReuseTest extends CommerceBrowserTestBase {
     $this->submitForm([], 'Checkout');
     $this->assertSession()->fieldValueEquals('billing_information[profile][address][0][address][given_name]', 'Frederick');
     $this->assertSession()->fieldValueEquals('billing_information[profile][address][0][address][family_name]', 'Pabst');
+    $this->assertSession()->fieldValueEquals('billing_information[profile][address][0][address][address_line1]', 'Pabst Blue Ribbon Dr');
+    $this->assertSession()->fieldValueEquals('billing_information[profile][address][0][address][postal_code]', '53177');
+    $this->assertSession()->fieldValueEquals('billing_information[profile][address][0][address][locality]', 'Milwaukee');
+    $this->assertSession()->fieldValueEquals('billing_information[profile][address][0][address][administrative_area]', 'WI');
+    $this->submitForm([], 'Continue to review');
+    $this->submitForm([], 'Complete checkout');
+    $this->assertSession()->pageTextContains('Your order number is 1. You can view your order on your account page when logged in.');
+
+    // Load profiles. The user should still have one profile.
+    $profiles = $profile_storage->loadMultipleByUser($this->testCustomer, 'customer', TRUE);
+    $this->assertCount(1, $profiles);
+    $customer_profile = reset($profiles);
+
+    $order = Order::load(1);
+    /** @var \Drupal\profile\Entity\ProfileInterface $order_billing_profile */
+    $order_billing_profile = $order->getBillingProfile();
+    $this->assertNotEquals($customer_profile->id(), $order_billing_profile->id());
+
+    // The address should be the same.
+    $customer_address = $customer_profile->get('address')->first()->toArray();
+    $order_billing_address = $order_billing_profile->get('address')->first()->toArray();
+    $this->assertEquals($customer_address, $order_billing_address);
+
+    $this->assertEquals(0, $order_billing_profile->getOwnerId());
   }
 
 }
