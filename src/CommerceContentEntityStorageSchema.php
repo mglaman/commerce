006e80042ac12bc8497fabf22a071f4cc54c97b5
joelpittet <joelpittet@160302.no-reply.drupal.org>
2021-01-06 15:52:21 +0200

    Issue #3183315 by joelpittet, Berdir: Update schema for new indexes.

--- src/CommerceContentEntityStorageSchema.php
+++ src/CommerceContentEntityStorageSchema.php
@@ -14,26 +14,6 @@ class CommerceContentEntityStorageSchema extends SqlContentEntityStorageSchema {
   /**
    * {@inheritdoc}
    */
-  public function onEntityTypeUpdate(EntityTypeInterface $entity_type, EntityTypeInterface $original) {
-    parent::onEntityTypeUpdate($entity_type, $original);
-
-    // ::onEntityTypeUpdate will only populate new indexes if the entity has
-    // a change on being translatable, revisionable, or a field change.
-    // @see https://www.drupal.org/project/drupal/issues/3005447
-    $entity_schema = $this->getEntitySchema($entity_type, TRUE);
-    $schema_table = $entity_type->getDataTable() ?: $entity_type->getBaseTable();
-    $schema_indexes = $entity_schema[$schema_table]['indexes'];
-    foreach ($schema_indexes as $index_name => $index_fields) {
-      if (!$this->database->schema()->indexExists($schema_table, $index_name)) {
-        $this->database->schema()
-          ->addIndex($schema_table, $index_name, $index_fields, $entity_schema[$schema_table]);
-      }
-    }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   protected function getSharedTableFieldSchema(FieldStorageDefinitionInterface $storage_definition, $table_name, array $column_mapping) {
     $schema = parent::getSharedTableFieldSchema($storage_definition, $table_name, $column_mapping);
 
